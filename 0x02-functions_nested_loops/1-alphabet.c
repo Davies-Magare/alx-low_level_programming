cc9bfe1eb76f08a1d8d61a01399642ec4a5804f8
Davies-magare <davieskamanda@gmail.com>
2022-07-05 16:35:24 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabet, in lowercase, followed by a new line.
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void) /* prints alphabet in lowercase*/
+{
+	char c;
+
+	for (c = 'a'; c <= z; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+	return (0);
+}
+
+
