d6da16daf55821a5568b1cdf2cd09505a119dd74
Davies-magare <davieskamanda@gmail.com>
2022-07-06 05:39:39 -0700

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,17 @@ void times_table(void)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			prod = i*j;
-			_putchar((prod / 10) + '0');
-			_putchar((prod % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
+			prod = i * j;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+			_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
 
 	}
-}	
+}
 
