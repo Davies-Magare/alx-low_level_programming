68fb0ff4f7c66b4db7f6e5b0583de42b990d3daa
Davies-magare <davieskamanda@gmail.com>
2022-07-05 18:43:35 -0700

    corrections

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,16 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		return (1);
+		_putchar('+');
 	{
-	_putchar('+');
 	if else (n == 0)
 	{
 		return (0);
+		_putchar('0');
 	}
-	_putchar('0');
 	else
 	{
 		return (-1);
+		_putchar('-');
 	}
-	_putchar('-1');
 }
