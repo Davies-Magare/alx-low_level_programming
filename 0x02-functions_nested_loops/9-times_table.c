8700d54ad544fcc2e0cf9d3d8db8469d947b8ee8
Davies-magare <davieskamanda@gmail.com>
2022-07-06 05:02:30 -0700

    a correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			prod = i*j;
-			_putchar(prod);
+			_putchar((prod % 10) + '0');
 			_putchar('\t');
 			_putchar(',');
 			_putchar(' ');
