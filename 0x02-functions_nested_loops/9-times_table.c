485a22c821a91a3ee341c24a561ab8c9c54b1d7a
Davies-magare <davieskamanda@gmail.com>
2022-07-06 04:58:52 -0700

    task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - prints 9 times table, starting with 0
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i, j, prod;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			prod = i*j;
+			_putchar(prod);
+			_putchar('\t');
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+
+	}
+}	
+
