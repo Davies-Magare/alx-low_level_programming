4bcf16753cb7e91839ad5a30a83c2da4be9fe129
Davies-magare <davieskamanda@gmail.com>
2022-07-11 15:19:30 -0700

    attempt at 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ * @s: The string whose length is to be determined
+ *
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int *s[100];
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		_putchar[i];
+	}
+}
