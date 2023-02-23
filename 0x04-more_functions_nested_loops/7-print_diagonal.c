3f391fa527583bf2c645ede8f2a40fc21c21103b
Davies-Magare <davieskamanda@gmail.com>
2023-02-23 01:40:00 -0800

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line in the terminal
+ * @n: The integer length of the diagonal line.
+ *
+ * Return: Nothing.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
