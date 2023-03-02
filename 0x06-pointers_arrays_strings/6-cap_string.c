d5a6905e400b1edc5bea3888efd186a765795a41
Davies-Magare <davieskamanda#@gmail.com>
2023-03-01 20:20:54 -0800

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all the words of a string
+ * @s: A string.
+ *
+ * Return: A pointer to the string.
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+	int punctuationMarks[13] = {
+		44, 59, 46, 33, 63,
+		34, 40, 41, 123, 125,
+		32, 9, 10,
+	}
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == punctuationMarks[j])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+					s[i + 1] = s[i + 1] - 32;
+			}
+		}
+	}
+	return (s);
+}
