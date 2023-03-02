f4269fcea6a3ddd3e694999dac237da0890f2549
Davies-Magare <davieskamanda#@gmail.com>
2023-03-01 22:18:51 -0800

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,28 +1,29 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all the words of a string
- * @s: A string.
+ * cap_string - capitalizes all the words of  a string
+ * @s: A pointer to a string.
  *
- * Return: A pointer to the string.
+ * Return: On success (s).
  */
 char *cap_string(char *s)
 {
 	int i, j;
-	int punctuationMarks[13] = {
-		44, 59, 46, 33, 63,
-		34, 40, 41, 123, 125,
-		32, 9, 10,
-	};
-
+	int punctuationMarks[13] = {44, 59, 46, 33, 63, 34, 40, 41, 123, 125,
+					32, 9, 10};
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < 13; j++)
+		if (i == 0 && (s[i] >= 97 && s[i] <= 122))
+			s[i] = s[i] - 32;
+		else
 		{
-			if (s[i] == punctuationMarks[j])
+			for (j = 0; j < 13; j++)
 			{
-				if (s[i + 1] >= 97 && s[i + 1] <= 122)
-					s[i + 1] = s[i + 1] - 32;
+				if (s[i] == punctuationMarks[j])
+				{
+					if (s[i + 1] >= 97 && s[i + 1] <= 122)
+						s[i + 1] = s[i + 1] - 32;
+				}
 			}
 		}
 	}
