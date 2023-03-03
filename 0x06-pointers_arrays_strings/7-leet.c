23c3ec48d0c8920cacd1fd2522b885e332ece45e
Davies-Magare <davieskamanda#@gmail.com>
2023-03-02 19:25:50 -0800

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @str: A string.
+ *
+ * Return: A pointer to str.
+ */
+char *leet(char *str)
+{
+	char replace[5] = {'4', '3', '0', '7', '1'};
+	char replaced[10] = {'A', 'E', 'O', 'T', 'L', 'a', 'e',
+				'o', 't', 'l'};
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == replaced[j])
+				str[i] = replace[j % 5];
+			else
+				continue;
+		}
+	}
+	return (str);
+}
