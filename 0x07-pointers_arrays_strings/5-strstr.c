c56588f35734bab2c048d4028c7f48f1c86ee1e7
Davies-Magare <davieskamanda#@gmail.com>
2023-03-07 08:00:03 -0800

    task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,38 +13,33 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, lengthOfHaystack, lengthOfNeedle, k, p;
+	int i, j, k, len;
 
-	for (i = 0; haystack[i] != '\0'; i++)
-		lengthOfHaystack++;
+	len = 0;
 	for (i = 0; needle[i] != '\0'; i++)
-		lengthOfNeedle++;
-	for (i = 0; i <= lengthOfHaystack; i++)
+		len++;
+	for (i = 0; haystack[i]; i++)
 	{
-		if (i == lengthOfHaystack)
-			return (NULL);
-		for (j = 0; j <= lengthOfNeedle; j++)
-		{
-			if (needle[j] == haystack[i])
-			{
-				p = i;
-				for (k = 0; k <= lengthOfNeedle; k++, p++)
-				{
-					if (k == lengthOfNeedle)
-						return (haystack + i);
-					if (needle[k] != haystack[p])
-					{
-						if (k < lengthOfNeedle - 1)
-							return (NULL);
-						else
-							return (haystack + 1);
-					}
-					continue;
-				}
-
-			}
+		if (!(needle[0]))
 			break;
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i] == needle[0])
+			{
+				for (k = 1; needle[k]; k++)
+				{
+					if (haystack[i + k] != needle[k])
+						break;
+					else
+					{
+						if (k < len - 1)
+							continue;
+						else
+							return (haystack + i);
+					}
+				}
+			}
 		}
 	}
-	return (haystack + i);
+	return (NULL);
 }
