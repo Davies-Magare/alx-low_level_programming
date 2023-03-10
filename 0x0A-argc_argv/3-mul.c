111cae2e704392b53869b055fe5eeefbeac03023
Davies-Magare <davieskamanda#@gmail.com>
2023-03-10 11:06:08 -0800

    task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: The integer argument count to the main function.
+ * @argv: A pointer to an array of string arguments passed to the main
+ * function.
+ *
+ * Return: Always 0;
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc == 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	mul = 1;
+	for (i = 1; i < argc; i++)
+	{
+		mul *= atoi(argv[i]);
+	}
+	printf("%i\n", mul);
+	return (0);
+}
