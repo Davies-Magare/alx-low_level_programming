e7b3e45a5e2e4f89febfa19cf8b9aa528825d3b7
Davies-Magare <davieskamanda#@gmail.com>
2023-03-13 21:25:10 -0700

    duplicating a string in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * which contains a copy of the string given as a parameter.
+ * @str: The string to be duplicated in memory.
+ *
+ * Return: On Success, a pointer to the duplicated string.
+ * Otherwise, return NULL.
+ */
+char *_strdup(char *str)
+{
+	int i, len;
+	char *dup;
+
+	if (str == NULL)
+		return (NULL);
+	len = 0;
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+	dup = malloc(len + 1);
+	if (dup == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+		dup[i] = str[i];
+	dup[i] = '\0';
+	return (dup);
+}
