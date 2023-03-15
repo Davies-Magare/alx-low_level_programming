7992f78d90ecb495306acde84f4cb827902dda7c
Davies-Magare <davieskamanda#@gmail.com>
2023-03-14 21:00:40 -0700

    Creating a 2d array dynamically

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: The width of the array.
+ * @height: The height of the array.
+ *
+ * Return: On Success, a pointer to a 2 dimensional array of integers
+ * Otherwise, return NULL.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **A;
+
+	A = NULL;
+	A = malloc(height * sizeof(int *));
+	if (A == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+		A[i] = malloc(width * sizeof(int));
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			A[i][j] = 0;
+		}
+	}
+	return (A);
+}
