94a85e9c3d16a2d8cdd06b84ed3a76958ea8c056
Davies-Magare <davieskamanda#@gmail.com>
2023-03-16 21:46:00 -0700

    Corrected code

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		A[i] = s1[i];
 	for (j = 0; j < n; j++)
 		A[i + j] = s2[j];
-	for (; j < n; j++)
-		A[i + j] = '\0';
+	A[i + j] = '\0';
 	return (A);
 }
