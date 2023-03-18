4268911edb22f414099a0c26f2bbaa3017bb469d
Davies-Magare <davieskamanda#@gmail.com>
2023-03-17 19:08:56 -0700

    Fixed code error

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	A = NULL;
-	A - malloc(nmemb * size);
+	A = malloc(nmemb * size);
 	if (A == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
