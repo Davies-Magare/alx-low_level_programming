b5cb0e2f549bfc7a6def669a7775f0fcbc2a223e
Davies-Magare <davieskamanda#@gmail.com>
2023-03-20 10:13:34 -0700

    Defining a struct type

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * struct dog - a structure type
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * Description: This is a new struct type with three
+ * members: name, age and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *name;
+}
+
+#endif
