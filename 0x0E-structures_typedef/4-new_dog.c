e41b22687c37296202eb2b7851948c8d427d5c09
Davies-Magare <davieskamanda#@gmail.com>
2023-03-21 23:45:11 -0700

    Fixed code errors

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,14 +19,39 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t new_dog;
 	dog_t *dogs = &new_dog;
+	char *namecpy;
+	char *ownercpy;
 
+	namecpy = malloc(20);
+	ownercpy = malloc(20);
+	if (namecpy != NULL)
+		_strcpy(namecpy, name);
+	if (ownercpy != NULL)
+		_strcpy(ownercpy, owner);
 	if (dogs != NULL)
 	{
-		dogs->name = name;
+		dogs->name = namecpy;
 		dogs->age = age;
-		dogs->owner = owner;
+		dogs->owner = ownercpy;
 		return (dogs);
 	}
 	else
 		return (NULL);
+}
+
+/**
+ * _strcpy - copies a string to a memory location
+ * @dest: The location into which to copy the string
+ * @src: The string to be copied
+ *
+ * Return: A pointer to the copied string in memory.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
 }
