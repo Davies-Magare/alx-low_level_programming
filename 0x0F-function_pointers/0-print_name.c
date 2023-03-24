603d734161700e1b5ad8ae1379b28f0165149a2e
Davies-Magare <davieskamanda#@gmail.com>
2023-03-23 22:00:30 -0700

    included header file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_name - prints a name
@@ -10,6 +11,6 @@
  */
 void print_name(char *name, void(*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name != NULL &&  f != NULL)
 		f(name);
 }
