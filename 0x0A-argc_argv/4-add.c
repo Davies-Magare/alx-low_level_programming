27e6c86fb414740b76e5e5dd560091af8f78d5f3
Davies Magare <davieskamanda@gmail.com>
2023-06-09 09:07:28 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: Count of arguments to main
+ * @argv: An array of pointers to string arguments to main
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int sum, index, status;
+
+	sum = 0;
+	for (index = 1; index < argc; index++)
+	{
+		if (argc == 1)
+		{
+			printf("%i\n", 0);
+			break;
+		}
+		status = atoi(argv[index]);
+		if (status)
+			sum += atoi(argv[index]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%i\n", sum);
+	return (0);
+}
+
+
