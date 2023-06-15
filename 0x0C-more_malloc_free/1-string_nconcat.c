f99fc2f2f90f79aba935efb9ce61b33b2531b50a
Davies Magare <davieskamanda@gmail.com>
2023-06-15 02:11:44 -0700

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concantenates n bytes of s2 to s1
+ * @s1: A string
+ * @s2: A second string
+ * @n: The number of bytes of s2 to concantenate to s1
+ *
+ * Return: On success, a pointer to an allocated space in
+ * memory with s1 and n bytes of s2 null terminated
+ * Otherwise, a pointer to NULL is returned
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i;
+	unsigned int j;
+	char *s;
+	int len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
+	s = malloc(len1 + n + 1);
+	if (s == NULL)
+		return (NULL);
+	strcpy(s, s1);
+	strncat(s, s2, n);
+	return (s);
+}
