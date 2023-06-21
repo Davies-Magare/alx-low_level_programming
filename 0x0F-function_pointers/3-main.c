52edf2f389e01fd8b0540e7b35fbad4a27189e2b
Davies Magare <davieskamanda@gmail.com>
2023-06-21 08:48:16 -0700

    Remove extra if statement

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,10 +25,7 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	ptr = get_op_func(argv[2]);
-	if (ptr != NULL)
-	{
-		result = ptr(atoi(argv[1]), atoi(argv[3]));
-		printf("%i\n", result);
-	}
+	result = ptr(atoi(argv[1]), atoi(argv[3]));
+	printf("%i\n", result);
 	return (0);
 }
