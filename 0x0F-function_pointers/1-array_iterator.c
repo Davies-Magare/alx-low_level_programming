98caa1adc3b3c1e95736569db33f93d6858d761e
Davies Magare <davieskamanda@gmail.com>
2023-06-21 04:01:02 -0700

    Manipulate an array with a fucntion pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * array_iterator - executes a function given as parameter
+ * on each element of the array.
+ * @array: An array.
+ * @size: The size of the array.
+ * @action: A pointer to the function to act on each element of array.
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
