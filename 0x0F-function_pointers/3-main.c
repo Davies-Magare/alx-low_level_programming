03816bea3470eb5dc38360168c8edb14aeca6a53
Davies Magare <davieskamanda@gmail.com>
2023-06-21 09:04:23 -0700

    Fixed code style

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,24 +2,32 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - driver function for a simple calculator
+ * @argc: The argument count
+ * @argv: Array of pointers to arguments passed to main
+ *
+ * Return: Always 0 (Success);
+ */
 int main(int argc, char *argv[])
 {
 	int (*ptr)(int, int);
-	int result;
+	int result, status;
 	char op;
-	
+
 	if (argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	sscanf(argv[2], "%c", &op);
-	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%')
+	status = sscanf(argv[2], "%c", &op);
+	if (op != '+' && op != '-' && op != '*' && op != '/' && op != '%'
+			&& status == 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((atoi(argv[3])== 0) && (op == '/' || op == '%'))
+	if ((atoi(argv[3]) == 0) && (op == '/' || op == '%'))
 	{
 		printf("Error\n");
 		exit(100);
