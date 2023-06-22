795fdc777fe7a01029fdb950b9c64e1d6bc6f62c
Davies Magare <davieskamanda@gmail.com>
2023-06-22 02:35:44 -0700

    Added case for n equal 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,12 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list num;
 	unsigned int i;
 	int number;
-
+	
+	if (n == 0)
+	{
+		printf("\n");
+		return (0);
+	}
 	va_start(num, n);
 	for (i = 0; i < n; i++)
 	{
