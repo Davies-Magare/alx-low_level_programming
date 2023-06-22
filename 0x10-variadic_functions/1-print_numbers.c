6e67ba0bde7d60606b6628aa040b9672584e05ee
Davies Magare <davieskamanda@gmail.com>
2023-06-22 02:32:58 -0700

    Print variable number parameters

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: The string to be printed between the numbers
+ * @n: The number of variable arguments.
+ *
+ * Return: Nothing.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list num;
+	unsigned int i;
+	int number;
+
+	va_start(num, n);
+	for (i = 0; i < n; i++)
+	{
+		number = va_arg(num, int);
+		if (i < n - 1)
+		{
+			printf("%i", number);
+			if (separator != NULL)
+				printf("%s", separator);
+		}
+		else
+			printf("%i\n", number);
+	}
+	va_end(num);
+}
