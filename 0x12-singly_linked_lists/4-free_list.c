2832edf9d59d941a874fde28ea17bd440fb797f9
Davies Magare <davieskamanda@gmail.com>
2023-07-01 20:56:30 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: The start of the list
+ *
+ * Return: Nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *curptr;
+	list_t *nextptr;
+
+	curptr = head;
+	while(curptr != NULL)
+	{
+		nextptr = curptr->next;
+		free(curptr->str);
+		free(curptr->next);
+		curptr = nextptr;
+	}
+	free(head);
+}
