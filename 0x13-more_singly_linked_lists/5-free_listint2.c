1da54c4014c29711c92524c4e3d87627e58de1cb
Davies Magare <davieskamanda@gmail.com>
2023-07-04 04:11:41 -0700

    Free listint_t 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ * @head: The start of the list
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+	listint_t *ptrnxt;
+
+	ptr = *head;
+	while(ptr != NULL)
+	{
+		ptrnxt = ptr->next;
+		free(ptr);
+		ptr = ptrnxt;
+	}
+	*head = NULL;
+}
