7c3013aabb80ef3b068b0341f477cacb7c40c450
Davies Magare <davieskamanda@gmail.com>
2023-07-03 21:33:45 -0700

    Task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a listint_t list
+ * @h: The beginning of the list
+ *
+ * Return: The number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *ptr;
+	size_t count;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
