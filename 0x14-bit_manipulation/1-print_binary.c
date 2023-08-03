1ebe1d54c7e3db400e4d10c2992beeaba78bc798
Davies Magare <davieskamanda@gmail.com>
2023-08-03 06:35:43 -0700

    Print a number in binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: A number
+ *
+ * Return: Nothing
+ */
+void print_binary(unsigned long int n)
+{
+	int i;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		for (i = 31; i >= 0; i--)
+		{
+			if (!(n & (1 << i)))
+				continue;
+			else
+				break;
+		}
+		for(; i >= 0; i--)
+		{
+			if (n & (1 << i)
+				_putchar('1');
+			else
+				_putchar('0');
+		}
+	}
+}
