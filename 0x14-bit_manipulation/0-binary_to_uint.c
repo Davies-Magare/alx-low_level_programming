dbd82cbd3cb31b1effe270cb047ec2e7372f59c9
Davies Magare <davieskamanda@gmail.com>
2023-08-04 01:51:30 -0700

    Fixed case for NULL string

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,13 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i, dec, len, number;
 
+	if (b == NULL)
+		return (0);
 	len = strlen(b) - 1;
 	dec = 0;
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0' && b[i] != '1') || b == NULL)
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 		number = (b[i] - 48) << len--;
 		dec += number;
