7a5abb0a62433aeb149cf5268ecc18d3c352356f
Davies Magare <davieskamanda@gmail.com>
2023-08-07 03:42:12 -0700

    Task 3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,10 +3,13 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #define BUFSIZE 1024
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void close_descriptor(int fd);
 
 
 #endif
