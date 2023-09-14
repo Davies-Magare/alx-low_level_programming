394763e452b156458f15e49beac5e4c350f5d687
Davies Magare <davieskamanda@gmail.com>
2023-09-14 10:27:50 -0700

    Task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		count++;
 		temp = temp->next;
 	}
-	if (temp == NULL)
+	if (count < idx)
 		return (NULL);
 	else if (temp->next == NULL)
 	{
