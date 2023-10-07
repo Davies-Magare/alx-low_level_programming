db010818a4a5319942321d9d17d821639e591cf1
Davies Magare <davieskamanda@gmail.com>
2023-10-07 08:49:22 +0000

    Taksk 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: The hash table
+ * 
+ * Return: Nothing
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int i;
+	
+	if (ht != NULL)
+	{
+		for (i = 0; i < ht->size; i++)
+		{
+			free_element(ht->array[i]);
+		}
+		free(ht->array);
+		free(ht);
+		ht = NULL;
+	}
+}
+void free_element(hash_node_t *list)
+{
+	hash_node_t *temp;
+
+	while (list != NULL)
+	{
+		if (list->key != NULL)
+			free(list->key);
+		if (list->value != NULL)
+			free(list->value);
+		temp = list->next;
+		free(list);
+		list = temp;
+	}
+}
